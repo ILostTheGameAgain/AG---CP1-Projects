56d2204d2b56b502bd5f1c4a3dd28c93a40f169c
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-05 09:23:02 -0600

    created new project

---
+++ whoAreYou/main.py
@@ -0,0 +1,5 @@
+# Alec George SkillPractice: Who Are You?
+name = input("\nWhat is your name? ")
+age = input("\nWhat is your age? (years) ")
+color = input("\nWhat is your favorite color? ")
+print("\nYour name is "+ name +", you are "+ age +" years old, and your favorite color is "+ color +".")
