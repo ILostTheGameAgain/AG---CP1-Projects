16a11a705614f0dd83eb460e610f233f178bf3e5
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-10 09:17:37 -0600

    new project

---
+++ celsiusToFahrenheit/main.py
@@ -0,0 +1,7 @@
+#Alec George Proficiency Test: Celsius to Fahrenheit
+
+celsius = int(input("\nwhat is the temperature? (celsius): "))
+
+fahrenheit = int((celsius*9/5)+32)
+
+print("\nwhen the temperature is "+str(celsius)+" degrees celcius,\nthe temperature is "+str(fahrenheit)+" degrees fahrenheit.\n")
