925cf615b5a274fd75b39ff1c3641065791e6404
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-17 09:19:20 -0600

    fixed an error

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,7 +1,8 @@
-#Alec George SkillPractice: Palandrom (is this supposed to be misspelled?)
+#Alec George SkillPractice: Palandrom
 
 word = input("What word would you like to test? ").lower().strip()
-if word[0] == word[::-1]:
+reverse_word = word[::-1].lower().strip()
+if word == reverse_word:
     print(word+" is a palindrome.")
 else:
     print(word+" isn't a palindrome.")
