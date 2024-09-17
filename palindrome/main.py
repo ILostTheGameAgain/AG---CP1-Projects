e6d66b3e4e2a76c0f58445c707be196513237693
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-17 09:26:57 -0600

    gave set values to variables

--- palindrome/main.py
+++ palindrome/main.py
@@ -1,8 +1,86 @@
 #Alec George SkillPractice: Palandrom
 
-word = input("What word would you like to test? ").lower().strip()
-reverse_word = word[::-1].lower().strip()
-if word == reverse_word:
-    print(word+" is a palindrome.")
+word = "mom"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
 else:
-    print(word+" isn't a palindrome.")
+    print(word+" isn't a palindrome.\n")
+
+word = "a"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "Dad"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "bed"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "racecar"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "civic"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "word"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "step on no pets"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "crab"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "borrow or rob"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "Yes!"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "radar"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = "tom"
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
+word = input("what word would you like to try? ").strip()
+if word == word[::-1]:
+    print(word+" is a palindrome.\n")
+else:
+    print(word+" isn't a palindrome.\n")
+
