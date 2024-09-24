2751d8922172d48a129b1db526d136590e538ee1
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-24 09:51:27 -0600

    completed personal information converter started anagram creator

---
+++ anagramCreator/main.py
@@ -0,0 +1,9 @@
+#Alec George Raid: Anagram Creator
+
+#needs import random
+import random
+
+#make it a function to repeat it
+def createAnagram(word):
+    list_of_letters = []
+createAnagram(input("What word would you like to create anagrams of? "))
---
+++ personalInformationConverter/main.py
@@ -0,0 +1,25 @@
+#Alec George Proficiency Test: Personal Information Converter
+
+#variables
+name = input("what is your name? ")
+age = input("how old are you? ")
+height = input("how tall are you? (meters): ")
+favorite_number = input("what is your favorite number? ")
+
+#print the results
+print("\nname:")
+print(name)
+print("name as a string (which it already is):")
+print(str(name))
+print("\nage: ")
+print(age)
+print("age as an integer:")
+print(int(age))
+print("\nheight:")
+print(height)
+print("height as a float:")
+print(float(height))
+print("\nfavorite number:")
+print(favorite_number)
+print("favorite number as an integer:")
+print(int(favorite_number))
