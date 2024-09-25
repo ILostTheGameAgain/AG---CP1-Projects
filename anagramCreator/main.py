1ef628279884f37b2a277d3be94f05955317fffa
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-25 09:36:37 -0600

    finished anagram creator

--- anagramCreator/main.py
+++ anagramCreator/main.py
@@ -2,8 +2,24 @@
 
 #needs import random
 import random
-
 #make it a function to repeat it
 def createAnagram(word):
-    list_of_letters = []
+    times = 0
+    anagram = ""
+    anagrams = []
+    while times < 5:
+        list_of_letters = []
+        for x in word:
+            list_of_letters.append(x)
+        anagram = ""
+        for x in word:
+            letter = random.randint(0, len(list_of_letters)-1)
+            anagram += list_of_letters[letter]
+            list_of_letters.pop(letter)
+        anagrams.append(anagram)
+        times += 1
+    print("\noriginal word: "+word+"\nanagrams:")
+    for x in anagrams:
+        print(x)
+    createAnagram(input("\nWhat word would you like to create anagrams of? "))
 createAnagram(input("What word would you like to create anagrams of? "))
