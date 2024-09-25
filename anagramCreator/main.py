192667d54634d8aecba53de26b73be2adfa7d5f6
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-25 09:50:40 -0600

    added comments made user input to choose number of anagrams created

--- anagramCreator/main.py
+++ anagramCreator/main.py
@@ -4,22 +4,31 @@
 import random
 #make it a function to repeat it
 def createAnagram(word):
+    #variable for number of anagrams
+    number_of_anagrams = input("How many anagrams do you want? ")
+    #variables that reset when function starts
     times = 0
     anagram = ""
     anagrams = []
-    while times < 5:
+    #angagram creator that repeats a number of times, once for each anagram
+    while times < int(number_of_anagrams):
+        #put every letter in a list
         list_of_letters = []
         for x in word:
             list_of_letters.append(x)
         anagram = ""
+        #rearange the letters in the list to form new anagrams
         for x in word:
             letter = random.randint(0, len(list_of_letters)-1)
             anagram += list_of_letters[letter]
             list_of_letters.pop(letter)
         anagrams.append(anagram)
         times += 1
+    #print the results
     print("\noriginal word: "+word+"\nanagrams:")
     for x in anagrams:
         print(x)
+    #allow the user to make more anagrams
     createAnagram(input("\nWhat word would you like to create anagrams of? "))
+#call function for first time
 createAnagram(input("What word would you like to create anagrams of? "))
