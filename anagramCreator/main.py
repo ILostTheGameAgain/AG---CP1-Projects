a664406a6ef45727bb980731ee401bee2c9fd040
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-09-26 09:51:33 -0600

    deleted random clone of folder inside of folder

--- anagramCreator/main.py
+++ anagramCreator/main.py
@@ -3,7 +3,12 @@
 #needs import random
 import random
 
+#list of letters to randomize them
+list_of_letters = []
 #make it a function to repeat it
 def createAnagram(word):
     list_of_letters = []
+    for x in 0,len(word),:
+        list_of_letters.append("e")
+    print(list_of_letters)
 createAnagram(input("What word would you like to create anagrams of? "))
