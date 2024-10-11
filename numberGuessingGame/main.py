225f00c156b6916a5a011727bd8787fffc2e22c1
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-10-11 09:51:40 -0600

    started number guessing game

---
+++ numberGuessingGame/main.py
@@ -0,0 +1,9 @@
+#Alec George Raid: Number guessing game
+
+
+#program will get a random number between one and ten, and the user keeps guessing numbers until they get it right, it will tell them too high or too low, and will tell them how many guesses it took to get it right
+
+#to get random number, needs random library
+import random
+
+#needs variables for random number, user input, possible inputs, already guessed inputs
