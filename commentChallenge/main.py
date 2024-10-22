755dec6681c42cd48d391b9a0c9c866bcae3ee65
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-10-22 09:25:13 -0600

    finished comment challenge

---
+++ commentChallenge/main.py
@@ -0,0 +1,13 @@
+#---------------------------------------------------
+# Name:     Comment Challenge
+# Purpose:  asks user for their name and says hi
+#
+# Author:   Alec George
+#
+# Created:  10/22/2024
+# Licence:  (none)
+#---------------------------------------------------
+
+name = input("What is your name? ") #assigns the name variable to user input, asking for their name
+
+print(f"hello, {name}") #displays the word hello, followed by the user's name
