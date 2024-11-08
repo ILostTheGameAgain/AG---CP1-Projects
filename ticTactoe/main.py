6ffee788c509bb8784072b227f86babf1079dae9
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-11-08 09:37:31 -0700

    finished tic tac toe

--- ticTactoe/main.py
+++ ticTactoe/main.py
@@ -1,19 +1,99 @@
 #Alec George Tic Tac Toe
 
-# play tic tac toe on adjustable board
+# play tic tac toe
 
-#adjustable board
-while True:
-    try:
-        board_size = int(input("\nhow large is the board? (squares): "))
+#random computer input
+import random
+
+#title
+print("""
++-----------+   +-----------+     ------          +-----------+        /\\           ------
+|           |   |           |    / /  \\ \\         |           |       /  \\         / /  \\ \\
++---+   +---+   +---+   +---+   / /    +-+        +---+   +---+      / /\\ \\       / /    +-+
+    |   |           |   |       | |                   |   |         / /__\\ \\      | | 
+    |   |       +---+   +---+   \\ \\    +-+            |   |        / ______ \\     \\ \\    +-+
+    |   |       |           |    \\ \\  / /             |   |       / /      \\ \\     \\ \\  / /
+    +---+       +-----------+     ------              +---+      +-+        +-+     ------
+
++-----------+      ------      +-----------+     
+|           |     / /  \ \\     |   ._______|
++---+   +---+    / /    \ \\    |   |____.
+    |   |        | |    | |    |        |
+    |   |        \ \\    / /    |   +----+
+    |   |         \ \\  / /     |   +-------+
+    +---+          ------      |___________|\n\n""")
+#set up board
+board_squares = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+#function to check if someone wins
+def win():
+    for x in range(3): #check rows
+        if board_squares[x][0] == board_squares[x][1] == board_squares[x][2]:
+            return True
+    for x in range(3): #check columns
+        if board_squares[0][x] == board_squares[1][x] == board_squares[2][x]:
+            return True
+    #check diagonals
+    if board_squares[0][0] == board_squares[1][1] == board_squares[2][2]:
+        return True
+    if board_squares[0][2] == board_squares[1][1] == board_squares[2][0]:
+        return True
+    return False
+
+
+#function to do an action
+def action(number, figure):
+    global board_squares
+    if number in board_squares[0]:
+        board_squares[0][number-1] = figure
+        return True
+    elif number in board_squares[1]:
+        board_squares[1][number-4] = figure
+        return True
+    elif number in board_squares[2]:
+        board_squares[2][number-7] = figure
+        return True
+    else:
+        if figure == "X":
+            print("that square is taken")
+        return False
+
+
+#function to print board
+def board():
+    global board_squares
+    print(f""" {board_squares[0][0]} | {board_squares[0][1]} | {board_squares[0][2]}
+---+---+---
+ {board_squares[1][0]} | {board_squares[1][1]} | {board_squares[1][2]}
+---+---+---
+ {board_squares[2][0]} | {board_squares[2][1]} | {board_squares[2][2]}""")
+
+
+while True: #keep playing until win
+    #print board
+    board()
+    while True:
+        try:
+            if action(int(input("\n what square? (type the number): ")), "X"):
+                break
+        except ValueError:
+            print("invalid input")
+
+    #check if user wins
+    if win():
+        board()
+        print("X wins")
         break
-    except ValueError:
-        print("invalid input")
 
-#setup board
-board = []
-for x in range(board_size):
-    board.append([])
-    for y in range(board_size):
-        board[x].append("  ")
+    #computer chooses random square
+    while True:
+        if action(random.randint(1,9), "O"):
+            break
+    
+    #check if computer wins
+    if win():
+        board()
+        print("O wins")
+        break
 
+print("thanks for playing!")
