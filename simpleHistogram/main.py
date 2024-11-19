0e952dc4ac3cc2f4246df9692dbbe7fb1a0bf10b
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-11-19 09:14:44 -0700

    finished simple "histogram"

---
+++ simpleHistogram/main.py
@@ -0,0 +1,127 @@
+# Alec George Raid: Simple histogram
+
+#will ask for a list of numbers and display the list, IT IS NOT ACTUALLY A HISTOGRAM!!!
+
+
+while True:
+    try:
+        inputted_numbers = input("\nwhat numbers would you like to display? (minimum of 6, separated by spaces)\n")
+        loop_repeats = 0
+
+        #reset variables: list and indexes for numbers to be put into lists
+        min_index = 0
+        max_index = 0
+        list_of_numbers = []
+      
+        for i in inputted_numbers: #use spaces to put numbers in list
+            if i == " ":
+                min_index = max_index
+                max_index = loop_repeats
+                list_of_numbers.append(int(inputted_numbers[min_index:max_index]))
+            loop_repeats += 1
+          
+        if inputted_numbers[-1] != " ": #put last number in list
+            min_index = max_index
+            max_index = len(inputted_numbers)
+            list_of_numbers.append(int(inputted_numbers[min_index:max_index]))
+          
+        if len(list_of_numbers) >= 6: #check if list is long enough
+            break
+            
+        else:
+            print("\nmust have at least 6 numbers")
+            
+    except ValueError:
+        print("\ninvalid input")
+
+#will display values as:
+#display 1's as *
+#for 10's, will use ^
+#for 100's will use %
+#for 1000's will use $
+#for 10000's will use #
+#for 100000's will use @
+#for 1000000's will use !
+#for 10000000's will use ~
+#and so on
+
+
+loop_repeats = 0
+
+for i in list_of_numbers:
+    next_number = i
+    next_number_translated = ""
+
+    while next_number >= 1000000000000:
+        next_number_translated += "?"
+        next_number -= 1000000000000
+
+    while next_number >= 100000000000:
+        next_number_translated += "E"
+        next_number -= 100000000000
+
+    while next_number >= 10000000000:
+        next_number_translated += ">"
+        next_number -= 10000000000
+
+    while next_number >= 1000000000:
+        next_number_translated += ":"
+        next_number -= 1000000000
+
+    while next_number >= 100000000:
+        next_number_translated += "|"
+        next_number -= 100000000
+
+    while next_number >= 10000000:
+        next_number_translated += "~"
+        next_number -= 10000000
+
+    while next_number >= 1000000:
+        next_number_translated += "!"
+        next_number -= 1000000
+
+    while next_number >= 100000:
+        next_number_translated += "@"
+        next_number -= 100000
+
+    while next_number >= 10000:
+        next_number_translated += "#"
+        next_number -= 10000
+
+    while next_number >= 1000:
+        next_number_translated += "$"
+        next_number -= 1000
+
+    while next_number >= 100:
+        next_number_translated += "%"
+        next_number -= 100
+
+    while next_number >= 10:
+        next_number_translated += "^"
+        next_number -= 10
+    
+    while next_number > 0:
+        next_number_translated += "*"
+        next_number -= 1
+    
+    loop_repeats += 1
+    print(str(loop_repeats) +": "+ next_number_translated)
+
+if input("\nshow key? (y/n): ") == "y":
+    #print key
+    print("""
+KEY:
+?: 1,000,000,000,000
+E: 100,000,000,000
+>: 10,000,000,000
+:: 1,000,000,000
+|: 100,000,000
+~: 10,000,000
+!: 1,000,000
+@: 100,000
+#: 10,000
+$: 1,000
+%: 100
+^: 10
+*: 1
+""")
