1002b2daf2fe9e83505e928cc5e3888ed3b00cad
ILostTheGameAgain <alec.george@ucas-edu.net>
2024-12-11 09:32:27 -0700

    worked on this a little

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,57 +1,71 @@
 #Alec George Final Project
 
+import random
+
 #first variables
 map = [[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "]]
-player_position = [[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," ","X"," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "]]
-player_x = 2
-player_y = 2
+player_position = [[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," ","E"," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "],[" "," "," "," "," "," "," "]]
+player_x = 3
+player_y = 3
+
+#setup board
+for y in range(7):
+    for x in range(7):
+        resource = random.randint(1,5)
+        if x == 3:
+            if y == 2:
+                resource = 6
+        if resource == 4:
+            map[y][x] = "oO"
+        elif resource == 5:
+            map[y][x] = "/\\"
+        elif resource == 6:
+            map[y][x] = "$ "
+        else:
+            map[y][x] = "  "
 #function for movement
 def movement(max_moves):
     global player_position
     global player_x
     global player_y
-    #list for the change of player position, [n, e, s, w]
+    #list for the change of player position, [n/s, e/w]
     position_change = [0, 0, 0, 0]
-    for y in range(7):
-        for x in range(7):
-            if player_position[y][x] == "X":
-                player_x = x
-                player_y = y
-    while True:
-        #user types letters, and moves depending on what they put
-        player_moves = input(f"where would you like to move? type up to {max_moves} letters,\nn for north\ne for east\ns for south\nw for west\nyour answer here\n").strip().lower()
-        if len(player_moves) <= max_moves: #user can't move more than an amount
-            for i in player_moves:
-                if i == "n":
-                    position_change[0] += 1
-                elif i == "e":
-                    position_change[1] += 1
-                elif i == "s":
-                    position_change[2] += 1
-                elif i == "w":
-                    position_change[3] += 1
-                else:
-                    print("invalid input")
-                    break
+    for i in range(max_moves):
+        while True:
+            #user types letters, and moves depending on what they put
+            player_moves = input(f"where would you like to move? type up to {max_moves} letters,\nn for north\ne for east\ns for south\nw for west\nyour answer here\n").strip().lower()
+            if len(player_moves) == 1: #user moves 1 place at a time
+                for i in player_moves:
+                    if i == "n":
+                        position_change[0] -= 1
+                    elif i == "e":
+                        position_change[1] += 1
+                    elif i == "s":
+                        position_change[0] += 1
+                    elif i == "w":
+                        position_change[1] -= 1
+                    else:
+                        print("invalid input")
+                        break
             
-            player_position[player_y][player_x] = " " #remove the previous x
-            #adjust position
-            player_x += position_change[1]
-            if player_x > 6:
-                player_x = 6
-            player_x -= position_change[3]
-            if player_x < 9:
-                player_x = 0
-            player_y += position_change[2]
-            if player_y > 6:
-                player_y = 6
-            player_y -= position_change[0]
-            if player_x < 0:
-                player_x = 0
-            player_position[player_y][player_x]= "X"
-            return True
-        else:
-            print("invalid input")
+                player_position[player_y][player_x] = " " #remove the previous x
+                #adjust position
+                player_y += position_change[0]
+                player_x += position_change[1]
+                if player_x > 6:
+                    player_x = 6
+                if player_x < 0:
+                    player_x = 0
+                if player_y > 6:
+                    player_y = 6
+                if player_y < 0:
+                    player_y = 0
+                player_position[player_y][player_x]= "E"
+                return True
+            else:
+                print("invalid input")
+
+
 
 #function to make the map
 def display_map():
@@ -74,7 +88,7 @@ def display_map():
     for y in range(7):
         print(f"|",end="")
         for x in range(7):
-            print(f"{map[y][x]} ", end="")
+            print(f"{map[y][x]}", end="")
             print(f" {player_position[y][x]}|", end="")
 
         if y == 0:
@@ -83,8 +97,16 @@ def display_map():
 
         else:
             print("\n+----+----+----+----+----+----+----+")
+    print("""KEY:
+$ - Shop       Oo - rocks
+# - treasure   /\\ - tree
+E - you
+""")
 
 
-display_map()
-movement(3)
-display_map()
+
+
+#run everythng
+while True:
+    display_map()
+    movement(3)
